a93b0a3d9c8616f9d3c1476b94c0692f9b82fdf9
Dirk Gadsden <dirk@esherido.com>
2013-03-08 16:25:04 -0500

    Add rply dep (from GitHub)

---
+++ deps/rply/token.py
@@ -0,0 +1,28 @@
+class BaseBox(object):
+    _attrs_ = []
+
+
+class Token(BaseBox):
+    def __init__(self, name, value, source_pos=None):
+        self.name = name
+        self.value = value
+        self.source_pos = source_pos
+
+    def __eq__(self, other):
+        return self.name == other.name and self.value == other.value
+
+    def gettokentype(self):
+        return self.name
+
+    def getsourcepos(self):
+        return self.source_pos
+
+    def getstr(self):
+        return self.value
+
+
+class SourcePosition(object):
+    def __init__(self, idx, lineno, colno):
+        self.idx = idx
+        self.lineno = lineno
+        self.colno = colno
