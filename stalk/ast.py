b76835e2d880d0f999ac2406b36e9a2272109c60
Dirk Gadsden <dirk@esherido.com>
2013-03-10 03:27:40 -0400

    Improve syntax tree functionality; add proper return handling

--- stalk/ast.py
+++ stalk/ast.py
@@ -1,5 +1,11 @@
 from rply.token import BaseBox
 
+class ReturnException(Exception):
+    def __init__(self, ret):
+        self.ret = ret
+    def get_return(self):
+        return self.ret
+
 # Primitive base of the language is the S_List, a list of statements to be
 # executed.
 class S_List(BaseBox):
@@ -10,15 +16,24 @@ class S_List(BaseBox):
     def __repr__(self):
         return "s("+(",".join([n.__repr__() for n in self.nodes]))+")"
     def eval(self, scope):
-        for stmt in self.nodes:
-            stmt.eval(scope)
+        # TODO: Fix all these co-dependent import hacks.
+        from stalk.object import SL_Method
+        ret = None
+        try:
+            for stmt in self.nodes:
+                ret = stmt.eval(scope)
+                # If it's a plain method then add it to the scope.
+                if type(ret) == SL_Method:
+                    scope.define_method(ret)
+        except ReturnException as re:
+            return re.get_return()
+        return ret
 
 from rpython.rlib.listsort import make_timsort_class
 def _sig_comp(a, b):
-    return len(a) > len(b) # Descending order by length
+    # return len(a) > len(b) # Descending order by length
+    return len(a) < len(b) # Ascending order by length
 SigSort = make_timsort_class(lt = _sig_comp)
-
-
 
 class S_Statement(S_List):
     def __init__(self, exprs):
@@ -58,7 +73,13 @@ class S_Statement(S_List):
         except AttributeError as e:
             raise NotImplementedError("Unknown expression: "+str(expr.__class__))
         self.e_target = ev(scope)
-        
+    
+    def peeknext(self):
+        ei = self.e_ec + 1
+        if ei < self.e_end:
+            return self.expressions[ei]
+        else:
+            return None
     def next(self):
         self.e_ec += 1
         if self.e_ec < self.e_end:
@@ -72,19 +93,75 @@ class S_Statement(S_List):
         substatement = S_Statement(self.expressions[self.e_ec:])
         ret = substatement.eval(scope)
         return ret
-    def match_send(self):
+    def match_def(self):
+        sig = []
+        block = None
+        e = self.e_expr
+        et = type(e)
+        if not e:
+            return False
+        if et == S_Operator:
+            ide = self.peeknext()
+            if type(ide) == S_Identifier:
+                sig = [e, ide]
+                self.next()
+                block = self.next()
+                if type(block) != S_Block:
+                    raise Exception("Missing block: "+repr(block))
+            else:
+                raise Exception("Unexpected following operator: "+repr(ide))
+        if et == S_Identifier:
+            sig = [e]
+            block = self.next()
+            if type(block) != S_Block:
+                raise Exception("Missing block: "+repr(block))
+        # Didn't match the operator shorthand
+        if len(sig) == 0:
+            is_keyword = True
+            while e:
+                if not e:
+                    break
+                if is_keyword:
+                    if et == S_Keyword:
+                        sig.append(e)
+                    elif et == S_Block:
+                        block = e
+                        break
+                    else:
+                        raise Exception("Unexpected non-keyword: "+repr(e))
+                else:
+                    if et == S_Identifier:
+                        sig.append(e)
+                    else:
+                        raise Exception("Unexpected non-identifier: "+repr(e))
+                e = self.next()
+                et = type(e)
+                is_keyword = not is_keyword
+        if len(sig) == 0:
+            raise Exception("Missing method definition: "+repr(e))
+        # TODO: Fix all these co-dependent import hacks.
+        from stalk.object import SL_Method
+        self.e_target = SL_Method(sig, block, self.e_scope)
+        self.next()
+        return True
+        
+    def match_send(self, recv):
         from stalk.object import compile_signature
         
         # Reset the send data
         self.e_send_sig = None
         self.e_send_params = None
         
-        recv = self.e_target # Receiver of the call.
+        # if self.e_target:
+        #     recv = self.e_target # Receiver of the call.
+        # else:
+        #     recv = self.e_scope
         expr = self.e_expr
         if not expr:
             return False
         
-        sigs = [m.get_signature() for m in recv.methods.values()]
+        #sigs = [m.get_signature() for m in recv.methods.values()]
+        sigs = recv.get_signatures()
         
         if expr.__class__ == S_Identifier:
             for sig in sigs:
@@ -119,20 +196,21 @@ class S_Statement(S_List):
                 return False
             if is_keyword:
                 if expr.__class__ != S_Keyword:
-                    raise Exception("Unexpected non-keyword: "+expr.__repr__())
+                    # TODO: Decide whether this should be greedy or not
+                    break # <- non-greedy
+                    #raise Exception("Unexpected non-keyword: "+expr.__repr__())
                 _sigs = []
                 for sig in sigs:
-                    se = sig[si]
-                    # TODO: Return to this and make it just one if (currently
-                    #       the first one is just to appease PyPy).
-                    #if isinstance(expr, S_Keyword) and isinstance(se, S_Keyword):
-                    #    if expr.get_value() == se.get_value():
-                    if expr.eq(se):
-                            _sigs.append(sig)
-                    
-                    
-                    #/if
-                    #if expr.get_value() != se.get_value():
+                    if si < len(sig):
+                        se = sig[si]
+                        # TODO: Return to this and make it just one if (currently
+                        #       the first one is just to appease PyPy).
+                        #if isinstance(expr, S_Keyword) and isinstance(se, S_Keyword):
+                        #    if expr.get_value() == se.get_value():
+                        if expr.eq(se):
+                                _sigs.append(sig)
+                        #/if
+                        #if expr.get_value() != se.get_value():
                 sigs = _sigs
             else:
                 if expr.__class__ != S_Keyword:
@@ -151,7 +229,37 @@ class S_Statement(S_List):
             return True
         # Didn't match!
         return False
-        
+    def do_send(self):
+        if self.e_target:
+            target = self.e_target # Receiver of the call.
+        else:
+            target = self.e_scope
+        # Build up a signature for a send
+        sig_head_index = self.e_ec
+        matched = self.match_send(target)
+        if matched:
+            sig = self.e_send_sig
+            exprs = self.e_send_params
+            params = [expr.eval(self.e_scope) for expr in exprs]
+            ret = target.send(sig, params)
+            if ret:
+                self.e_target = ret
+            else:
+                raise Exception("Method not found")
+        else:
+            if self.e_ec >= self.e_end:
+                return # No signature found because end of statement
+            if sig_head_index < self.e_end:
+                sig_head = self.expressions[sig_head_index].__repr__()
+                if target:
+                    tr = repr(target)
+                else:
+                    tr = "unknown"
+                raise Exception("No method "+sig_head+" for "+tr)
+            else:
+                # Shouldn't ever reach here.
+                raise Exception("No method error")
+    
     def eval(self, scope):
         self.e_ec = 0 # Current position in the expression of evaluation
         self.e_end = len(self.expressions) # Length of expression
@@ -162,34 +270,31 @@ class S_Statement(S_List):
         self.e_send_params = None
         while self.e_ec < self.e_end:
             self.e_expr = self.expressions[self.e_ec] # Current expression it's on
-            if not self.e_target:
-                # Get the target if there isn't one.
-                self.set_target()
-            else:
-                # Build up a signature for a send
-                sig_head_index = self.e_ec
-                matched = self.match_send()
-                if matched:
-                    sig = self.e_send_sig
-                    exprs = self.e_send_params
-                    params = [expr.eval(scope) for expr in exprs]
-                    ret = self.e_target.send(sig, params)
-                    if ret:
-                        self.e_target = ret
-                    else:
-                        raise Exception("Method not found")
+            if type(self.e_expr) == S_Keyword:
+                if self.e_expr.get_value() == "def:":
+                    self.next()
+                    self.match_def()
+                if self.e_expr.get_value() == "return:":
+                    substatement = S_Statement(self.expressions[self.e_ec + 1:])
+                    ret = substatement.eval(self.e_scope)
+                    # return ret
+                    re = ReturnException(ret)
+                    raise re
+                else: # Not a def but still a top-level method call
+                    self.do_send()
+            if type(self.e_expr) == S_Identifier and self.e_expr.get_value() == "return":
+                if self.e_target:
+                    # return self.e_target
+                    re = ReturnException(self.e_target)
+                    raise re
                 else:
-                    if self.e_ec >= self.e_end:
-                        break # No signature found because end of statement
-                    if sig_head_index < self.e_end:
-                        sig_head = self.expressions[sig_head_index].__repr__()
-                        if self.e_target:
-                            target = repr(self.e_target)
-                        else:
-                            target = "unknown"
-                        raise Exception("No method "+sig_head+" for "+target)
-                    else:
-                        raise Exception("No method error")
+                    raise Exception("Return without expressions")
+            else:
+                if not self.e_target:
+                    # Get the target if there isn't one.
+                    self.set_target()
+                else:
+                    self.do_send()
             self.e_ec += 1
         # Teardown
         self.e_ec = 0
@@ -219,7 +324,7 @@ class S_Expression(BaseBox):
     def get_value(self):
         return str(self.value)
     def eval(self, scope):
-        raise NotImplementedError("Reached raw expression")
+        raise NotImplementedError("Reached raw expression: "+repr(self))
     def eq(self, other):
         return self == other
 
@@ -302,16 +407,19 @@ class S_Block(S_Expression):
     def __init__(self, _header, value):
         self.type = "block"
         # List of expressions to be evaluated for the block.
-        self.list = value
+        self.list = value # S_List
         self.header = _header
         self.preface = None
     def __repr__(self):
-        return "s_block()"
+        return "s_block("+repr(self.list)+")"
     def get_value(self):
         #return (self.preface, self.list)
         return "[block]"
     def set_preface(self, preface):
         self.preface = preface
+    def eval(self, scope):
+        ret = self.list.eval(scope)
+        return ret
 
 
 class S_Literal(S_Expression):
