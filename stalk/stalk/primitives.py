c3699075aca8434fb69568df5bcc782b9f04fac9
Dirk Gadsden <dirk@esherido.com>
2013-03-09 23:18:52 -0500

    Start cleanup of primitives

--- stalk/stalk/primitives.py
+++ stalk/stalk/primitives.py
@@ -9,39 +9,25 @@ def _int_add(method, args):
         return SL_Int(v)
     else:
         return SL_Exception("Cannot add "+args[0].get_name()+" to "+method.parent.get_name())
-int_addition = SL_Primitive_Method(
-    [S_Operator("+"), S_Identifier("other")],
-    _int_add
-)
+int_addition = SL_Primitive_Method([S_Operator("+"), S_Identifier("other")], _int_add)
+
 def _int_println(method, args): # args = empty
     print method.parent.__repr__()
     return method.parent
-int_println = SL_Primitive_Method(
-    [S_Identifier("println")],
-    _int_println
-)
+int_println = SL_Primitive_Method([S_Identifier("println")], _int_println)
 
 def _int_string(method, args): # args = empty
     return SL_String(method.parent.get_value_string())
-int_string = SL_Primitive_Method(
-    [S_Identifier("string")],
-    _int_string
-)
+int_string = SL_Primitive_Method([S_Identifier("string")], _int_string)
 
 def _string_println(method, args):
     print method.parent.get_value_string()
     return method.parent
-string_println = SL_Primitive_Method(
-    [S_Identifier("println")],
-    _string_println
-)
+string_println = SL_Primitive_Method([S_Identifier("println")], _string_println)
 
 def _string_string(method, args):
     return method.parent
-string_string = SL_Primitive_Method(
-    [S_Identifier("string")],
-    _string_string
-)
+string_string = SL_Primitive_Method([S_Identifier("string")], _string_string)
 
 def _string_add(method, args):
     if type(args[0]) == SL_String:
@@ -49,25 +35,16 @@ def _string_add(method, args):
         return SL_String(v)
     else:
         return SL_Exception("Cannot add "+args[0].get_name()+" to "+method.parent.get_name())
-string_add = SL_Primitive_Method(
-    [S_Operator("+"), S_Identifier("other")],
-    _string_add
-)
+string_add = SL_Primitive_Method([S_Operator("+"), S_Identifier("other")], _string_add)
 
 def _array_string(method, args):
     a = method.parent.get_value_array()
     sig = "string" #[S_Identifier("string")]
     ao = [o.send(sig, []).get_value_string() for o in a]
     return SL_String("["+",".join(ao)+"]")
-array_string = SL_Primitive_Method(
-    [S_Identifier("string")],
-    _array_string
-)
+array_string = SL_Primitive_Method([S_Identifier("string")], _array_string)
 
 def _array_println(method, args):
     print _array_string(method, [])
     return method.parent
-array_println = SL_Primitive_Method(
-    [S_Identifier("println")],
-    _array_println
-)
+array_println = SL_Primitive_Method([S_Identifier("println")], _array_println)
