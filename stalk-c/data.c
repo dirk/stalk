e0fde5c2c8500548a02dfadc101675c30434650f
Dirk Gadsden <dirk@esherido.com>
2013-03-11 16:57:36 -0400

    Add data

--- stalk-c/data.c
+++ stalk-c/data.c
@@ -1,4 +1,49 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 #import "stalk.h"
 #import "data.h"
+#import "symbol.h"
 
+static sl_obj_id sl_i_obj_id_counter = 0;
+static pthread_mutex_t sl_i_obj_id_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+static sl_d_sym_t* sl_i_sym_table = NULL;
+static pthread_rwlock_t sl_i_sym_table_lock = PTHREAD_RWLOCK_INITIALIZER;
+
+// Thread-safe object id generation.
+sl_obj_id sl_obj_next_id() {
+  sl_obj_id id;
+  assert(pthread_mutex_lock(&sl_i_obj_id_mutex) == 0);
+  id = sl_i_obj_id_counter;
+  sl_i_obj_id_counter += 1;
+  assert(pthread_mutex_unlock(&sl_i_obj_id_mutex) == 0);
+  return id;
+}
+
+sl_d_sym_t* sl_d_sym_new(char* name) {
+  sl_d_sym_t* s;
+  sl_sym_id sym_id = sl_i_str_to_sym_id(name);
+  assert(pthread_rwlock_rdlock(&sl_i_sym_table_lock) == 0);
+  HASH_FIND_INT(sl_i_sym_table, &sym_id, s);
+  pthread_rwlock_unlock(&sl_i_sym_table_lock);
+  if(s != NULL) {
+    printf("sym table hit\n");
+    return s;
+  }
+  printf("sym table miss\n");
+  s = malloc(sizeof(sl_d_sym_t));
+  assert(s != NULL);
+  s->type = SL_DATA_SYM;
+  s->id = sl_obj_next_id();
+  int len = strlen(name) + 1;
+  s->value = malloc(sizeof(char) * len);
+  strncpy(s->value, name, len); // dest, src
+  s->sym_id = sym_id;
+  assert(pthread_rwlock_wrlock(&sl_i_sym_table_lock) == 0);
+  HASH_ADD_INT(sl_i_sym_table, sym_id, s);
+  pthread_rwlock_unlock(&sl_i_sym_table_lock);
+  return s;
+}
--- stalk-c/data.h
+++ stalk-c/data.h
@@ -1,17 +1,49 @@
 #ifndef DATA_H
 #define DATA_H
 
-typedef unsigned char sl_data_type;
-typedef int sl_sym_id;
+#import "deps/uthash/src/uthash.h"
 
-#define SL_DATA_SYM (unsigned int)0
+// FNV hash of symbol string
+typedef unsigned int sl_sym_id;
+// Incremented for each object created
+typedef unsigned int sl_obj_id;
+
+typedef unsigned char sl_data_type;
+#define SL_DATA_OBJ   (sl_data_type)0
+#define SL_DATA_SYM   (sl_data_type)1
+#define SL_DATA_SCOPE (sl_data_type)2
 
 #define SL_DATA_TYPE sl_data_type type;
+#define SL_OBJ_ID    sl_obj_id id;
+
+typedef unsigned int sl_scope_item_id;
+
+typedef struct sl_d_obj {
+  SL_DATA_TYPE
+  SL_OBJ_ID
+} sl_d_obj_t;
 
 typedef struct sl_d_sym {
   SL_DATA_TYPE
-    sl_sym_id id;
+  SL_OBJ_ID
+  sl_sym_id sym_id;
   char *value;
+  UT_hash_handle hh;
 } sl_d_sym_t;
+
+typedef struct sl_i_scope_item {
+  sl_scope_item_id id;// Computed using sl_d_scope_hash_name(name)
+  char *name;
+  UT_hash_handle hh;
+} sl_i_scope_item_t;
+
+typedef struct sl_d_scope {
+  SL_DATA_TYPE
+  SL_OBJ_ID
+  sl_d_obj_t *parent;
+  sl_i_scope_item_t *locals;
+} sl_scope_t;
+
+sl_d_sym_t* sl_d_sym_new(char* name);
 
 #endif
