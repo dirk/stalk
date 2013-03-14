ccc82ca7aa08e9499f5a6b54200b1255fc91100b
Dirk Gadsden <dirk@esherido.com>
2013-03-13 22:55:45 -0400

    Add null and switch over to just slots

--- stalk-c/data.c
+++ stalk-c/data.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <assert.h>
 
@@ -16,6 +17,32 @@ static pthread_mutex_t sl_i_obj_id_mutex = PTHREAD_MUTEX_INITIALIZER;
 static sl_d_sym_t* sl_i_sym_table = NULL;
 static pthread_rwlock_t sl_i_sym_table_lock = PTHREAD_RWLOCK_INITIALIZER;
 
+// #define aliases SL_D_NULL and D_NULL to sl_d_null.
+// Throw in "extern sl_d_null_t* sl_d_null;" into any source files that need
+// this.
+sl_d_obj_t* sl_d_null = NULL;
+
+// OBJECTS --------------------------------------------------------------------
+
+sl_d_obj_t* sl_d_obj_new() {
+  sl_d_obj_t* obj;
+  obj = sl_d_gen_obj_new(SL_DATA_OBJ, sizeof(sl_d_obj_t));
+  return obj;
+}
+
+// Generic object creation with given type and size.
+// Sets type, id, parent, refcount, methods, and values attributes.
+void* sl_d_gen_obj_new(sl_data_type type, size_t size) {
+  sl_d_obj_t* o = malloc(size);
+  assert(o != NULL);
+  o->type     = type;
+  o->id       = sl_obj_next_id();
+  o->parent   = NULL;
+  o->refcount = 1;
+  o->slots    = NULL;
+  return o;
+}
+
 // Thread-safe object id generation.
 sl_obj_id sl_obj_next_id() {
   sl_obj_id id;
@@ -26,29 +53,40 @@ sl_obj_id sl_obj_next_id() {
   return id;
 }
 
-// Generic object creation with given type and size.
-// Sets type, id, parent, refcount, methods, and values attributes.
-void* sl_d_gen_obj_new(sl_data_type type, size_t size) {
-  sl_d_obj_t* o = malloc(size);
-  assert(o != NULL);
-  o->type = type;
-  o->id = sl_obj_next_id();
-  o->parent = NULL;
-  o->refcount = 1;
-  o->methods = NULL;
-  o->values = NULL;
-  return o;
-}
+// OBJECT MEMORY MANAGEMENT ---------------------------------------------------
 
-sl_d_obj_t* sl_d_obj_new() {
-  sl_d_obj_t* obj;
-  obj = sl_d_gen_obj_new(SL_DATA_OBJ, sizeof(sl_d_obj_t));
-  return obj;
+// Predefining before retain-release
+static inline void sl_d_obj_free(sl_d_obj_t* obj);
+
+void sl_d_obj_retain(sl_d_obj_t* obj) {
+  obj->refcount += 1;
+}
+bool sl_d_obj_release(sl_d_obj_t* obj) {
+  obj->refcount -= 1;
+  if(obj->refcount != 0) {
+    return false;
+  }
+  sl_d_obj_free(obj);
+  return true;
 }
 
 // Free that doesn't give a shit about the reference count.
-static inline void sl_d_obj_force_free(sl_d_obj_t* obj) {
-  // Do any specific frees
+static inline void sl_d_obj_free(sl_d_obj_t* obj) {
+  // Clear out slots.
+  if(obj->slots != NULL) {
+    sl_i_sym_item_t* current_item;
+    sl_i_sym_item_t* tmp_item;
+    // sl_d_obj_t* current_obj;
+    HASH_ITER(hh, obj->slots, current_item, tmp_item) {
+      HASH_DEL(obj->slots, current_item);
+      if(current_item->value != NULL) {
+        sl_d_obj_free((sl_d_obj_t*)current_item->value);
+      }
+      free(current_item);
+    }
+  }
+  
+  // Then call the internal free to finish the rest of the job.
   switch(obj->id) {
   case SL_DATA_ARRAY:
     sl_d_array_free((sl_d_array_t*)obj);
@@ -67,43 +105,51 @@ static inline void sl_d_obj_force_free(sl_d_obj_t* obj) {
   }
 }
 
-bool sl_d_obj_free(sl_d_obj_t* obj) {
-  obj->refcount -= 1;
-  if(obj->refcount != 0) {
-    return false;
+// MESSAGE SENDING ------------------------------------------------------------
+
+sl_d_obj_t* sl_d_exception_new(int count, char** strings) {
+  // Calculate the lengths
+  int lengths[count];
+  int total_length = 0;
+  int i = 0;
+  for(i = 0; i < count; i++) {
+    int length = strlen(strings[i]);
+    lengths[i] = length;
+    total_length += length;
   }
-  
-  // Clear out values and locals (and call sl_d_obj_free on each).
-  if(obj->values != NULL) {
-    sl_i_sym_item_t* current_item;
-    sl_i_sym_item_t* tmp_item;
-    // sl_d_obj_t* current_obj;
-    HASH_ITER(hh, obj->values, current_item, tmp_item) {
-      HASH_DEL(obj->values, current_item);
-      if(current_item->value != NULL) {
-        sl_d_obj_free((sl_d_obj_t*)current_item->value);
-      }
-      free(current_item);
-    }
+  total_length += 1;
+  // Concatenate the strings
+  char buff[total_length];
+  buff[0] = '\0';
+  for(i = 0; i < count; i++) {
+    strncat(buff, strings[i], lengths[i]);
   }
-  if(obj->methods != NULL) {
-    sl_i_sym_item_t* current_item;
-    sl_i_sym_item_t* tmp_item;
-    // sl_d_obj_t* current_obj;
-    HASH_ITER(hh, obj->methods, current_item, tmp_item) {
-      HASH_DEL(obj->methods, current_item);
-      if(current_item->value != NULL) {
-        sl_d_obj_free((sl_d_obj_t*)current_item->value);
-      }
-      free(current_item);
-    }
+  // Add trailing newline and null byte
+  // buff[total_length - 2] = '\0';
+  buff[total_length - 1] = '\0';
+  
+  sl_d_obj_t* e = sl_d_obj_new();
+  e->type = SL_DATA_EXCEPTION;
+  // Set message string on slot
+  sl_d_string_t* s = sl_d_string_new(buff);
+  sl_d_obj_set_slot(e, sl_d_sym_new("message"), (sl_d_obj_t*)s);
+  
+  DEBUG("exception: %s", buff);
+  
+  return e;
+}
+
+sl_d_obj_t* sl_d_obj_send(sl_d_obj_t* target, sl_d_message_t* msg) {
+  sl_d_method_t* method = (sl_d_method_t*)sl_d_obj_get_slot(target, msg->signature);
+  if(method == NULL || method->type != SL_DATA_METHOD) {
+    sl_d_sym_t* sig = msg->signature;
+    char* msgs[2] = {"Method not found: ", sig->value};
+    sl_d_obj_t* e = sl_d_exception_new(2, msgs);
+    return e;
   }
+  DEBUG("method = %p", method);
   
-  
-  // Then call the internal free to finish the rest of the job.
-  sl_d_obj_force_free(obj);
-  
-  return true;
+  return (sl_d_obj_t*)SL_D_NULL;
 }
 
 // METHOD ---------------------------------------------------------------------
@@ -112,10 +158,33 @@ sl_d_method_t* sl_d_method_new() {
   sl_d_method_t* m;
   m = sl_d_gen_obj_new(SL_DATA_METHOD, sizeof(sl_d_method_t));
   m->signature = NULL;
-  m->closure = NULL;
+  m->params = NULL;
   m->block = NULL;
   m->hint = NULL;
   return m;
+}
+
+// MESSAGE --------------------------------------------------------------------
+
+sl_d_message_t* sl_d_message_new() {
+  sl_d_message_t* m;
+  m = sl_d_gen_obj_new(SL_DATA_MESSAGE, sizeof(sl_d_message_t));
+  m->signature = NULL;
+  m->arguments = NULL;
+  return m;
+}
+void sl_d_message_empty(sl_d_message_t* m) {
+  if(m->arguments != NULL) {
+    sl_d_obj_release((sl_d_obj_t*)m->arguments);
+  }
+}
+
+// STRING ---------------------------------------------------------------------
+
+sl_d_string_t* sl_d_string_new(char* value) {
+  sl_d_string_t* s = sl_d_gen_obj_new(SL_DATA_STRING, sizeof(sl_d_string_t));
+  s->value = strdup(value);
+  return s;
 }
 
 // SCOPE ----------------------------------------------------------------------
@@ -129,56 +198,42 @@ void sl_d_scope_free(sl_d_scope_t* s) {
   free(s);
 }
 
-// TODO: De-duplicate this
-void sl_d_obj_set_value(sl_d_obj_t* obj, sl_d_sym_t* name, sl_d_obj_t* val) {
+void sl_d_obj_set_slot(sl_d_obj_t* obj, sl_d_sym_t* name, sl_d_obj_t* val) {
   sl_i_sym_item_t* check_item;
   sl_sym_id id = name->sym_id;
-  HASH_FIND_INT(obj->values, &id, check_item);
+  HASH_FIND_INT(obj->slots, &id, check_item);
   if(check_item == NULL) {
     sl_i_sym_item_t* item = malloc(sizeof(sl_i_sym_item_t));
     item->id = id;
     item->value = val;
-    HASH_ADD_INT(obj->values, id, item);
+    HASH_ADD_INT(obj->slots, id, item);
   } else {
     sl_d_obj_t* old_val = check_item->value;
     check_item->value = val;
     sl_d_obj_free(old_val);
   }
+  // Retain the value now that it's stored in this object.
+  sl_d_obj_retain(val);
 }
-void sl_d_obj_set_method(sl_d_obj_t* obj, sl_d_sym_t* name, sl_d_obj_t* val) {
-  sl_i_sym_item_t* check_item;
-  sl_sym_id id = name->sym_id;
-  HASH_FIND_INT(obj->methods, &id, check_item);
-  if(check_item == NULL) {
-    sl_i_sym_item_t* item = malloc(sizeof(sl_i_sym_item_t));
-    item->id = id;
-    item->value = val;
-    HASH_ADD_INT(obj->methods, id, item);
-  } else {
-    sl_d_obj_t* old_val = check_item->value;
-    check_item->value = val;
-    sl_d_obj_free(old_val);
-  }
-}
-sl_d_obj_t* sl_d_obj_get_value(sl_d_obj_t* obj, sl_d_sym_t* name) {
+sl_d_obj_t* sl_d_obj_get_slot(sl_d_obj_t* obj, sl_d_sym_t* name) {
   sl_i_sym_item_t* val;
   sl_sym_id id = name->sym_id;
-  HASH_FIND_INT(obj->values, &id, val);
+  HASH_FIND_INT(obj->slots, &id, val);
   if(val == NULL) {
     return NULL;
   } else {
     return val->value;
   }
 }
-sl_d_obj_t* sl_d_obj_get_method(sl_d_obj_t* obj, sl_d_sym_t* name) {
-  sl_i_sym_item_t* val;
-  sl_sym_id id = name->sym_id;
-  HASH_FIND_INT(obj->methods, &id, val);
-  if(val == NULL) {
-    return NULL;
-  } else {
-    return val->value;
-  }
+
+// BLOCK ----------------------------------------------------------------------
+
+sl_d_block_t* sl_d_block_new() {
+  sl_d_block_t* b = sl_d_gen_obj_new(SL_DATA_BLOCK, sizeof(sl_d_block_t));
+  b->expr = NULL;
+  b->closure = NULL;
+  b->params = NULL;
+  return b;
 }
 
 
@@ -193,8 +248,20 @@ sl_d_array_t* sl_d_array_new() {
   return arr;
 }
 void sl_d_array_free(sl_d_array_t* arr) {
+  sl_d_obj_t* obj;
+  for(
+    obj = (sl_d_obj_t*)utarray_front(arr->objs);
+    obj != NULL;
+    obj = (sl_d_obj_t*)utarray_next(arr->objs, obj)
+  ) {
+    sl_d_obj_release(obj);
+  }
   utarray_free(arr->objs);
   free(arr);
+}
+void sl_d_array_push(sl_d_array_t* arr, sl_d_obj_t* obj) {
+  sl_d_obj_retain(obj);
+  utarray_push_back(arr->objs, &obj);
 }
 
 // SYMBOL ---------------------------------------------------------------------
@@ -207,16 +274,16 @@ sl_d_sym_t* sl_d_sym_new(char* name) {
     HASH_FIND_INT(sl_i_sym_table, &sym_id, s);
   pthread_rwlock_unlock(&sl_i_sym_table_lock);
   if(s != NULL) {
-    DEBUG("sym table hit");
+    // DEBUG("sym table hit: %s", name);
     return s;
   }
-  DEBUG("sym table miss");
+  // DEBUG("sym table miss: %s", name);
   
   s = (sl_d_sym_t*)sl_d_gen_obj_new(SL_DATA_SYM, sizeof(sl_d_sym_t));
-  // value and length
-  s->length = len;
-  s->value = malloc(sizeof(char) * len);
-  strncpy(s->value, name, len); // dest, src
+  // value
+  // s->value = malloc(sizeof(char) * (len + 1));
+  // strncpy(s->value, name, len + 1); // dest, src
+  s->value = strdup(name);
   // sym_id
   s->sym_id = sym_id;
   // hh
@@ -224,6 +291,7 @@ sl_d_sym_t* sl_d_sym_new(char* name) {
     sl_d_sym_t* s_check;
     HASH_FIND_INT(sl_i_sym_table, &sym_id, s_check);
     if(s_check != NULL) {
+      // DEBUG("sym table dup: %s", name);
       // sl_d_sym_free without thread safety, since we already have write-lock.
       free(s->value);
       free(s);
@@ -251,7 +319,7 @@ void sl_d_sym_free(sl_d_sym_t* sym) {
 
 sl_d_int_t* sl_d_int_new() {
   sl_d_int_t* i;
-  i = sl_d_gen_obj_new(SL_DATA_ARRAY, sizeof(sl_d_int_t));
+  i = sl_d_gen_obj_new(SL_DATA_INT, sizeof(sl_d_int_t));
   return i;
 }
 void sl_d_int_free(sl_d_int_t* i) {
--- stalk-c/data.h
+++ stalk-c/data.h
@@ -12,25 +12,35 @@ typedef unsigned int sl_sym_id;
 typedef unsigned int sl_obj_id;
 
 typedef unsigned char sl_data_type;
-#define SL_DATA_OBJ    (sl_data_type)0
-#define SL_DATA_SYM    (sl_data_type)1
-#define SL_DATA_SCOPE  (sl_data_type)2
-#define SL_DATA_ARRAY  (sl_data_type)3
-#define SL_DATA_METHOD (sl_data_type)4
-#define SL_DATA_INT    (sl_data_type)5
+#define SL_DATA_OBJ       (sl_data_type)0
+#define SL_DATA_SYM       (sl_data_type)1
+#define SL_DATA_SCOPE     (sl_data_type)2
+#define SL_DATA_ARRAY     (sl_data_type)3
+#define SL_DATA_METHOD    (sl_data_type)4
+#define SL_DATA_INT       (sl_data_type)5
+#define SL_DATA_MESSAGE   (sl_data_type)6
+#define SL_DATA_BLOCK     (sl_data_type)7
+#define SL_DATA_NULL      (sl_data_type)8
+#define SL_DATA_EXCEPTION (sl_data_type)8
+#define SL_DATA_STRING    (sl_data_type)9
 
 #define SL_DATA_TYPE    sl_data_type type;
 #define SL_OBJ_ID       sl_obj_id id;
 #define SL_OBJ_PARENT   void* parent;
 #define SL_OBJ_REFCOUNT unsigned int refcount;
-#define SL_OBJ_METHODS  sl_i_sym_item_t* values;
-#define SL_OBJ_VALUES   sl_i_sym_item_t* methods;
+#define SL_OBJ_SLOTS    sl_i_sym_item_t* slots;
 #define SL_OBJ_HEADER   SL_DATA_TYPE \
                         SL_OBJ_ID \
                         SL_OBJ_PARENT \
                         SL_OBJ_REFCOUNT \
-                        SL_OBJ_METHODS \
-                        SL_OBJ_VALUES
+                        SL_OBJ_SLOTS
+
+#define SL_D_NULL sl_d_null
+#define D_NULL    sl_d_null
+
+#define DEBUG(M, ...) fprintf(stderr, "[DEBUG] %s:%s:%d: " M "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
+
+#define SL_DATA_EXTERN extern sl_d_obj_t* sl_d_null;
 
 typedef unsigned int sl_scope_item_id;
 
@@ -54,27 +64,14 @@ typedef struct sl_d_int {
 typedef struct sl_d_sym {
   SL_OBJ_HEADER;
   sl_sym_id sym_id;
-  char *value;//non-null-terminated string of length .length
-  unsigned char length;//length of value
+  char *value;//cstring
   UT_hash_handle hh;
 } sl_d_sym_t;
-
-typedef struct sl_d_block {
-  SL_OBJ_HEADER;
-  sl_s_expr_t* expr;
-  sl_s_list_t* params;
-} sl_d_block_t;
 
 typedef struct sl_d_array {
   SL_OBJ_HEADER;
   UT_array* objs;
 } sl_d_array_t;
-
-/*typedef struct sl_i_scope_item {
-  sl_scope_item_id id;// Computed using sl_d_scope_hash_name(name)
-  char *name;
-  UT_hash_handle hh;
-} sl_i_scope_item_t;*/
 
 typedef struct sl_d_scope {
   SL_OBJ_HEADER;
@@ -84,6 +81,21 @@ typedef struct sl_d_scope {
   // sl_i_scope_item_t *locals;
   // sl_i_sym_item_t* locals;
 } sl_d_scope_t;
+
+typedef struct sl_d_block {
+  SL_OBJ_HEADER;
+  sl_s_expr_t* expr;
+  sl_d_scope_t* closure;
+  sl_d_array_t* params;
+} sl_d_block_t;
+
+/*typedef struct sl_i_scope_item {
+  sl_scope_item_id id;// Computed using sl_d_scope_hash_name(name)
+  char *name;
+  UT_hash_handle hh;
+} sl_i_scope_item_t;*/
+
+
 
 #define SL_I_METHOD_F(NAME) sl_d_obj_t* NAME( \
   sl_d_obj_t* self, \
@@ -99,34 +111,55 @@ typedef SL_I_METHOD_F((*sl_i_method_f));
 typedef struct sl_d_method {
   SL_OBJ_HEADER;
   sl_d_sym_t* signature;
-  sl_d_scope_t* closure;
+  sl_d_array_t* params;
   sl_d_block_t* block;
   // Optional function pointer, currently used for just primitives but in the
   // future hopefully also for JITing.
   sl_i_method_f hint;
 } sl_d_method_t;
 
+typedef struct sl_d_message {
+  SL_OBJ_HEADER;
+  sl_d_sym_t* signature;
+  sl_d_array_t* arguments;
+} sl_d_message_t;
 
-
+typedef struct sl_d_string {
+  SL_OBJ_HEADER;
+  char* value;
+} sl_d_string_t;
 
 // FUNCTIONS ------------------------------------------------------------------
 
+void sl_d_bootstrap();
+
 sl_d_obj_t* sl_d_obj_new();
-bool sl_d_obj_free(sl_d_obj_t* obj);
-void sl_d_obj_set_value(sl_d_obj_t* obj, sl_d_sym_t* name, sl_d_obj_t* val);
-void sl_d_obj_set_method(sl_d_obj_t* obj, sl_d_sym_t* name, sl_d_obj_t* val);
-sl_d_obj_t* sl_d_obj_get_value(sl_d_obj_t* obj, sl_d_sym_t* name);
-sl_d_obj_t* sl_d_obj_get_method(sl_d_obj_t* obj, sl_d_sym_t* name);
+sl_obj_id sl_obj_next_id();
+void* sl_d_gen_obj_new(sl_data_type type, size_t size);
+void sl_d_obj_retain(sl_d_obj_t* obj);
+bool sl_d_obj_release(sl_d_obj_t* obj);
+// Methods and values
+void sl_d_obj_set_slot(sl_d_obj_t* obj, sl_d_sym_t* name, sl_d_obj_t* val);
+sl_d_obj_t* sl_d_obj_get_slot(sl_d_obj_t* obj, sl_d_sym_t* name);
+// Method sending
+sl_d_obj_t* sl_d_obj_send(sl_d_obj_t* target, sl_d_message_t* ret);
 
 sl_d_sym_t* sl_d_sym_new(char* name);
 void sl_d_sym_free(sl_d_sym_t* sym);
 
 sl_d_method_t* sl_d_method_new();
 
+sl_d_message_t* sl_d_message_new();
+
+sl_d_block_t* sl_d_block_new();
+
 sl_d_scope_t* sl_d_scope_new();
 void sl_d_scope_free(sl_d_scope_t* s);
 
+sl_d_string_t* sl_d_string_new(char* value);
+
 sl_d_array_t* sl_d_array_new();
+void sl_d_array_push(sl_d_array_t* arr, sl_d_obj_t* obj);
 void sl_d_array_free(sl_d_array_t* arr);
 
 sl_d_int_t* sl_d_int_new();
